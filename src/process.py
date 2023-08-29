a0e39ffdf6a68a9e2c089f60f5e88e8887a5f8b0
luzimu <luzimu@mail.ustc.edu.cn>
2023-08-29 15:07:45 +0800

    update process.py

--- src/process.py
+++ src/process.py
@@ -1,6 +1,17 @@
 import json
 import os
 from tqdm import tqdm
+import re
+import random
+
+def is_number(s):
+    pattern = r'^-?\d+(?:\.\d+)?$'
+    return bool(re.match(pattern, s))
+
+def save_jsonl(datas, file_name):
+    with open(file_name, "w", encoding="utf-8") as f:
+        for data in datas:
+            f.write(json.dumps(data, ensure_ascii=False) + "\n")
 
 def process(in_path, out_path, prefix):
     files = [f for f in os.listdir(in_path) if f.startswith(prefix) and os.path.isfile(os.path.join(in_path, f))]
@@ -51,5 +62,40 @@ def process(in_path, out_path, prefix):
     print(f"error_num: {error_num}")
     print(f"total_num: {total_num}")
 
+
+def process_test(in_path, out_path, prefix):
+    files = [f for f in os.listdir(in_path) if f.startswith(prefix) and os.path.isfile(os.path.join(in_path, f))]
+
+    with open(os.path.join(out_path, "k12_test.jsonl"), "w", encoding="utf-8") as f_out:
+        for file_name in files:
+            with open(os.path.join(in_path, file_name), "r", encoding="utf-8") as f:
+                datas = [json.loads(line) for line in f]
+                for idx, data in tqdm(enumerate(datas)):
+                    if is_number(data["answer"]):
+                        system = ""
+                        user = data["question"]
+                        assistant = data["solution"] + f"\n所以本题的答案为{data['answer']}. ### {data['answer']}"
+                        new_data = {"messages": [{"role": "system", "content": [{"type": "text", "content": system}]},
+                                                {"role": "user", "content": [{"type": "text", "content": user}]},
+                                                {"role": "assistant", "content": [{"type": "text", "content": assistant}]}]}
+                        f_out.write(json.dumps(new_data, ensure_ascii=False) + "\n")
+        
+
+def split(data, out_path):
+    # Shuffle the data
+    random.shuffle(data)
+
+    # Split the data into 90% train and 10% test
+    split_idx = int(0.9 * len(data))
+    train_data = data[:split_idx]
+    test_data = data[split_idx:]
+    save_jsonl(train_data, os.path.join(out_path, "train.jsonl"))
+    save_jsonl(test_data, os.path.join(out_path, "test.jsonl"))
+
 if __name__ == "__main__":
-    process("/mnt/cache/luzimu/qb_math_2023082801/qb_math_2023082801", "/mnt/cache/luzimu/qb_math_2023082801/qb_math_2023082801/outs", "pretrain_Math")
+    # process("/mnt/cache/luzimu/qb_math_2023082801/qb_math_2023082801", "/mnt/cache/luzimu/qb_math_2023082801/qb_math_2023082801/outs", "pretrain_Math")
+    # process_test("/mnt/cache/luzimu/qb_math_2023082801/qb_math_2023082801/outs", "/mnt/cache/luzimu/qb_math_2023082801/qb_math_2023082801/test", "out_pretrain_Math")
+    with open("/mnt/cache/luzimu/qb_math_2023082801/qb_math_2023082801/test/k12_test.jsonl", "r") as f:
+        datas = [json.loads(line) for line in f]
+    print(len(datas))
+    split(datas, "/mnt/cache/luzimu/qb_math_2023082801/qb_math_2023082801/test")
